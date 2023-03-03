7e68d7699db9ace568b02d4fc53008dfb1c5bc1a
HellHorseman <duke.sheffer@yandex.ru>
2023-03-03 22:56:42 +0400

    Add task 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static int[] generateRandomArray() {
@@ -16,29 +17,39 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         int[] arr = generateRandomArray();
-        int sumSalary = 0;
+        int sumSpending = 0;
         for (int j : arr) {
-            sumSalary += j;
+            sumSpending += j;
         }
-        System.out.println("Сумма трат за месяц составила " + sumSalary + " рублей.");
+        System.out.println("Сумма трат за месяц составила " + sumSpending + " рублей.");
     }
-
     public static void task2() {
         System.out.println("Задача 2");
         int[] arr = generateRandomArray();
         int maxSpending = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > maxSpending) {
-                maxSpending = arr[i];
+        for (int j : arr) {
+            if (j > maxSpending) {
+                maxSpending = j;
             }
         }
         System.out.println("Максимальная сумма трат за день составила "+ maxSpending +" рублей");
         int minSpending = 1_000_000;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < minSpending) {
-                minSpending = arr[i];
+        for (int j : arr) {
+            if (j < minSpending) {
+                minSpending = j;
             }
         }
         System.out.println("Минимальная сумма трат за день составила "+ minSpending +" рублей");
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] arr = generateRandomArray();
+        double midSpending = 0;
+        int sumSpending = 0;
+        for (int j : arr) {
+            sumSpending += j;
+        }
+        midSpending = sumSpending / 30;
+        System.out.println("Средняя сумма трат за месяц составила " + midSpending + " рублей.");
     }
 }
