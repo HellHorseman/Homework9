197566f336ccba85ff95526b30da2509654eec49
HellHorseman <duke.sheffer@yandex.ru>
2023-03-03 22:46:23 +0400

    Add task 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -11,18 +12,33 @@ public class Main {
         }
         return arr;
     }
-    public static void task1 () {
+
+    public static void task1() {
         System.out.println("Задача 1");
         int[] arr = generateRandomArray();
         int sumSalary = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sumSalary += arr[i];
+        for (int j : arr) {
+            sumSalary += j;
         }
         System.out.println("Сумма трат за месяц составила " + sumSalary + " рублей.");
     }
 
-    public static void task2 () {
+    public static void task2() {
         System.out.println("Задача 2");
-
+        int[] arr = generateRandomArray();
+        int maxSpending = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxSpending) {
+                maxSpending = arr[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила "+ maxSpending +" рублей");
+        int minSpending = 1_000_000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minSpending) {
+                minSpending = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила "+ minSpending +" рублей");
     }
 }
