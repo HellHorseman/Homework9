e4fd0249afed1f851d397dd5e820f0e1df0d5b81
HellHorseman <duke.sheffer@yandex.ru>
2023-03-03 22:10:59 +0400

    Add Random Array + task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sumSalary = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumSalary += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sumSalary + " рублей.");
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
     }
 }
